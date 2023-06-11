826ecf910319ff4ce5214227974069b3bba300bc
silostack <92659125+silostack@users.noreply.github.com>
2023-06-11 15:22:23 -0500

    fixed build

--- programs/yardsale/src/constant.rs
+++ programs/yardsale/src/constant.rs
@@ -9,3 +9,7 @@ pub const C_LISTINGS: &str = "clistings";
 
 // metadata (for metaplex nft metadata accounts)
 pub const METADATA: &str = "metadata";
+
+
+// first 8 bytes of SHA256("global:transfer")
+pub const TRANSFER_DISCRIMINATOR: &'static [u8;8] = &[163, 52, 200, 231, 140, 3, 69, 186];
