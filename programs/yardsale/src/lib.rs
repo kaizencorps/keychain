f036f693c7ab058dfb435db4ce476f5d82176826
silostack <92659125+silostack@users.noreply.github.com>
2023-06-11 15:18:51 -0500

    try solandy's method

--- programs/yardsale/src/lib.rs
+++ programs/yardsale/src/lib.rs
@@ -106,6 +106,58 @@ pub mod yardsale {
         index: u32,
     ) -> Result<()> {
 
+        msg!("attempting to send nft {} from tree {}", index, ctx.accounts.merkle_tree.key());
+
+        let mut accounts:  Vec<solana_program::instruction::AccountMeta> = vec![
+            AccountMeta::new_readonly(ctx.accounts.tree_authority.key(), false),
+            AccountMeta::new_readonly(ctx.accounts.leaf_owner.key(), true),
+            AccountMeta::new_readonly(ctx.accounts.leaf_owner.key(), false),
+            AccountMeta::new_readonly(ctx.accounts.new_leaf_owner.key(), false),
+            AccountMeta::new(ctx.accounts.merkle_tree.key(), false),
+            AccountMeta::new_readonly(ctx.accounts.log_wrapper.key(), false),
+            AccountMeta::new_readonly(ctx.accounts.compression_program.key(), false),
+            AccountMeta::new_readonly(ctx.accounts.system_program.key(), false),
+        ];
+
+        let mut data: Vec<u8> = vec![];
+        data.extend(TRANSFER_DISCRIMINATOR);
+        data.extend(root);
+        data.extend(data_hash);
+        data.extend(creator_hash);
+        data.extend(nonce.to_le_bytes());
+        data.extend(index.to_le_bytes());
+
+        let mut account_infos: Vec<AccountInfo> = vec![
+            ctx.accounts.tree_authority.to_account_info(),
+            ctx.accounts.leaf_owner.to_account_info(),
+            ctx.accounts.leaf_owner.to_account_info(),
+            ctx.accounts.new_leaf_owner.to_account_info(),
+            ctx.accounts.merkle_tree.to_account_info(),
+            ctx.accounts.log_wrapper.to_account_info(),
+            ctx.accounts.compression_program.to_account_info(),
+            ctx.accounts.system_program.to_account_info(),
+        ];
+
+        // add "accounts" (hashes) that make up the merkle proof
+        for acc in ctx.remaining_accounts.iter() {
+            accounts.push(AccountMeta::new_readonly(acc.key(), false));
+            account_infos.push(acc.to_account_info());
+        }
+
+        msg!("manual cpi call");
+        solana_program::program::invoke(
+            & solana_program::instruction::Instruction {
+                program_id: ctx.accounts.bubblegum_program.key(),
+                accounts: accounts,
+                data: data,
+            },
+            &account_infos[..])
+            // &[&[b"cNFT-vault", &[*ctx.bumps.get("leaf_owner").unwrap()]]])
+            .map_err(Into::into)
+
+
+        /*
+
         // remaining_accounts are the accounts that make up the required proof
         let remaining_accounts_len = ctx.remaining_accounts.len();
         let mut accounts = Vec::with_capacity(
@@ -172,6 +224,8 @@ pub mod yardsale {
         // solana_program::program::invoke(&instruction, &account_infos[..])?;
 
         Ok(())
+
+         */
 
         /* this way apparently doesn't work ..?   see: https://solana.stackexchange.com/questions/6410/anchor-cpi-bubblegum-burn-error-cause-not-signer
         // new with signer for pda signing
